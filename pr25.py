33b9a8a787aa3c1917135ba745e8ce891a6903a4
vishaltech7781 <vishalbiradar21412@gmail.com>
2025-09-10 13:56:51 +0530

    Added project files

--- pr25.py
+++ pr25.py
@@ -1,3 +1,30 @@
-num = int(input("enter the number :"))
+# *     1
+# **    12
+# ***   123
+# ****  1234
+# ***** 12345
 
-if 
+n = int(input("enter the number :"))
+
+def pattern(n):
+    for i in range(n+1):
+        for j in range(1,i+1):
+            print(j,end="")
+        print()
+
+pattern(n)
+
+# ***** 12345
+# ****  1234
+# ***   123
+# **    12
+# *     1
+n = int(input("enter the number :"))
+
+def pattern(n):
+    for i in range(n):
+        for j in range(i,n):
+            print(j,end="")
+        print()
+
+pattern(n)
